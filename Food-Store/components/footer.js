e7c8c3d5244da2f469d580bebcab77dd9cf4f157
Bugata Bala Murali <158822404+BalaMurali219@users.noreply.github.com>
2025-02-20 21:51:19 +0530

    Add files via upload

---
+++ Food-Store/components/footer.js
@@ -0,0 +1,18 @@
+export const footer = () => {
+    return (
+        `<div id="footer">
+            <p class="text-center">Made with ❤️ by Murali..</p>
+            <div>
+                <button class="btn btn-primary">
+                    <a href="" target="_blank">LinkedIn</a>
+                </button>
+                <button class="btn btn-success">
+                    <a href="" target="_blank">GitHub</a>
+                </button>
+                <button class="btn btn-danger">
+                    <a href="" target="_blank">Portfolio</a>
+                </button>
+            </div>
+        </div>`
+    );
+};
